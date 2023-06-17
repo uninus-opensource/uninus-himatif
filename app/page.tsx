49dc1b85815bf1c667a5c18317f1cbb803ca203c
Muhammad Luthfi Ramadhan <108562600+EL5225@users.noreply.github.com>
2023-06-17 22:25:50 +0700

    feature: add navbar (#15)
    * fix | design pattern
    
    * feature: add Navbar
    
    * edit navbar
    
    * feature: add Navbar
    
    * edit navbar
    
    * feature: add Navbar
    
    * edit navbar
    
    * feature: add navbar
    
    * fix: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactElement } from "react";
 import { TextField } from "@/components";
+import Link from "next/link";
 
 const Home: FC = (): ReactElement => {
   return (
     <section className="flex items-center">
       <h1 className="text-4xl text-blue-400">Test</h1>
+      <Link href="/about">About</Link>
     </section>
   );
 };
